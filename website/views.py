38a25cd9c8ef899729c0b1bd51eaae47c99b537c
JaredLucas22 <102346087+JaredLucas22@users.noreply.github.com>
2024-09-19 20:56:48 +0800

    Sponsorlist

--- website/views.py
+++ website/views.py
@@ -26,18 +26,10 @@ def recommendation():
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+    # Query to get all sponsorships
+    sponsorships = Sponsorship_data.query.all()
 
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
-
-    return render_template("home.html", user=current_user)
+    return render_template("home.html", user=current_user, sponsorships=sponsorships)
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
